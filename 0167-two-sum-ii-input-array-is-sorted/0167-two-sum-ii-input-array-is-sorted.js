ddd4a88d23e52993d26150578447fb5cb8d86011
DEEP CHOUDHURY <sanandachoudhury69@gmail.com>
2026-07-13 09:20:01 +0530

    Time: 3 ms (35.55%), Space: 56.6 MB (33.35%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i=0,j=numbers.length-1;
+
+    while(i<j){
+        if ((numbers[i]+numbers[j]) === target){
+            return [i+1,j+1];
+        }
+        else if((numbers[i] + numbers[j]) < target){
+            i++;
+        }
+        else if((numbers[i] + numbers[j]) > target){
+            j--;
+        }
+        
+    }
+
+    return[0,0];
+
+};
