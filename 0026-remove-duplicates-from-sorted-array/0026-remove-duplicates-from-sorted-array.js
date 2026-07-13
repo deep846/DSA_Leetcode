5f988c92d67d8af91e28f35ba189002d52e46da8
DEEP CHOUDHURY <sanandachoudhury69@gmail.com>
2026-07-13 17:33:52 +0530

    Time: 0 ms (100%), Space: 59.2 MB (5.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let i=0,j=i+1,k=1;
+    let i=0,j=i+1;
     while (j<nums.length){
         if(nums[i]!==nums[j]){
             nums[i + 1] = nums[j];
             i++;
             j++;
-            k++;
         }
         else{
             j++;
         }
         if(j===nums.length){
             // console.log(nums.slice(0,i+1))
-            return k;
+            return ++i;
         }
     }
 
