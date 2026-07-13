3a178e4aa55326fee886089ce002d9db672a0255
DEEP CHOUDHURY <sanandachoudhury69@gmail.com>
2026-07-13 19:09:30 +0530

    Time: 5 ms (60.87%), Space: 63.4 MB (6.66%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -1,19 +1,91 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var sortedSquares = function(nums) {
+//     let j=0;
+//     while(j<nums.length){
+
+//             nums[j] = nums[j]*nums[j];
+//             j++;
+        
+//     }
+
+//         nums.sort(function(a, b) {
+//     return a - b;
+//     });
+
+//     return nums;
+// };
+
+
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let j=0;
-    while(j<nums.length){
+    let i=0;
 
-            nums[j] = nums[j]*nums[j];
-            j++;
-        
+    while(nums[i]<0){
+        i++;
     }
 
-        nums.sort(function(a, b) {
-    return a - b;
-    });
+    if(i>0){
+        let negArray = nums.slice(0,i);
+        let posArray = nums.slice(i,nums.length);
+        negArray = squreArray(negArray);
+        posArray = squreArray(posArray);
+
+        if(negArray.length==0){
+            return posArray;
+        }
+        else if(posArray.length==0){
+            return negArray.reverse();
+        }
+        nums =[];
+        // console.log(negArray,posArray);
+        let n=negArray.length - 1;
+        let p = 0;
+
+        while(n>=0 || p<posArray.length){
+
+            if(n>=0 && p<posArray.length){
+
+                if(negArray[n]>=posArray[p]){
+                    nums.push(posArray[p])
+                    p++;
+                }
+                else if(negArray[n]<posArray[p]){
+                    nums.push(negArray[n])
+                    n--;
+                }
+            }
+            else if(n<0){
+                nums.push(...posArray.slice(p,posArray.length));
+                p = posArray.length;
+            }
+            else if(p>=posArray.length){
+                nums.push(...negArray.slice(0,n+1).reverse());
+                n = -1;
+            }
+        }
+
+        // console.log(nums);
+        return nums;
+    }
+    else{
+        return squreArray(nums);
+    }
+    
+
+};
+
+
+var squreArray = function (nums){
+    for(let i=0; i<nums.length;i++){
+        nums[i] = nums[i]*nums[i];
+
+    }
 
     return nums;
-};
+}
